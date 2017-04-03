ddd56d365416276592dd9560c5ba970c85da1718
Roman Shuvaryk <roman_shuvaryk@epam.com>
2017-04-03 09:04:31 +0300

    Fixed middleware

--- server.js
+++ server.js
@@ -17,13 +17,14 @@ const forceSSL = function() {
 
 app.use(forceSSL());
 
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
